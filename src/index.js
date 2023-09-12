b1504a4d493e87ffce750750a91f6d4b773236ea
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-09-12 23:16:53 +0200

    fix crash build

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <BrowserRouter basename="/searchcar">
         <App />
       </BrowserRouter>
